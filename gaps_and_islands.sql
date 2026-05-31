31f5177423ea5f3e4129c5b6b1e211fc4b4555be
g-muchiri <nmuchirig@gmail.com>
2026-05-31 12:07:50 +0300

    We cannot progress with this data since there are no hiring streaks. We load a new dataset and restart

--- gaps_and_islands.sql
+++ gaps_and_islands.sql
@@ -93,5 +93,51 @@ select * from gaps_and_islands where end_date >start_date;
 delete from gaps_and_islands 
 where end_date < start_date;
 
---our data now makes sense
+--If our data had a sense of mutiple employees switching departments over the years
+--how we'd have captured s this was
 
+select 
+employee_id, 
+start_date, 
+lag(start_date)over(partition by employee_id  order by start_date) as next_role_start_date
+from gaps_and_islands;
+
+/*The Querry above just proved further that if we have a question of continuity, 
+ * it will shift from continous state of employee status to operations continuity
+ * 
+ * The Operations department wants to audit its historical staffing footprint. 
+ * They want to know the continuous "islands" of time where the department had 
+ * active data entries, and where the "gaps" are 
+ * (periods of time where no operations records started or existed)
+ * 
+ * */
+
+--Looking at the data we have made a conclusion that there was no single streak
+--lets try to prove it
+with ranked_hires as(
+	select 
+		employee_id, 
+		start_date, 
+		row_number()over(order by start_date desc) as ranker
+	from gaps_and_islands
+),
+anchored as (
+	select 
+		employee_id, 
+		start_date, 
+		(start_date - interval '1 day'*ranker) as streak
+	from ranked_hires 
+)
+select * from anchored where streak >1;
+
+with ranked_hires as(
+	select 
+		employee_id, 
+		start_date, 
+		row_number()over(order by start_date desc) as ranker
+	from gaps_and_islands
+)
+select employee_id, start_date, (start_date - interval '1 day'*ranker) as streak
+	from ranked_hires;
+
+select * from gaps_and_islands order by end_date desc;
