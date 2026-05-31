f214428e5552507f96c8c0e9515c5f29434f9bc8
g-muchiri <nmuchirig@gmail.com>
2026-05-31 15:01:47 +0300

    Challenge completed Check the code out

--- logs_gaps.sql
+++ logs_gaps.sql
@@ -4,7 +4,7 @@ alter table logs_data
 rename column "Login date" to login_date;
 
 update logs_data ld 
-set new_login_date =
+set login_date =
 case
 	when login_date ~ '^(0[1-9]|1[0-9]|2[0-9]|3[0-1])/(0[1-9]|1[0-2]|[0-9])/[0-9]{4}$'then to_date(login_date, 'DD/MM/YYYY')--DD/MM/YYYY
 	when login_date ~ '^(0[1-9]|1[0-2]|[0-9])/(0[1-9]|1[0-9]|2[0-9]|3[0-1])/[0-9]{4}$'then to_date(login_date, 'MM/DD/YYYY')--MM/DD/YY
@@ -23,4 +23,50 @@ select * from logs_data ld where new_login_date is null;
 alter table logs_data 
 drop column if exists new_login_date;
 
+
+/*
+ * ****** Phase Two****
+ * ***Gaps and Islands Phase***
+ * 
+ * Find the longest consecutive daily streak for each user
+ * 
+ * */
+
+alter table logs_data  
+rename column "Client _ID" to client_id;
+
+--This standard helps accept other designs of data eg'YYYY,DD,MM'
+SET datestyle = 'ISO, MDY';
+
+alter table logs_data 
+alter column login_date type date
+using login_date::date;
+
+with grouped_logs as (
+select 
+	client_id, 
+	login_date, 
+	lag(login_date)over(partition by client_id order by login_date) prev_login_date,
+	row_number()over(partition by client_id)row_num
+from logs_data
+),
+anchored as (
+select 
+	client_id, 
+	login_date, 
+	prev_login_date,
+	(login_date - interval '1 day'*row_num) as anchor
+from grouped_logs
+)--in the next querry you will be counting the anchor
+select
+	client_id,
+	count(*) as Streak_of_days
+from anchored
+group by client_id, anchor 
+order by client_id ;
+
+
+
+
 select * from logs_data ld ;
+
